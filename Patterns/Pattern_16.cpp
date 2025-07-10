603155f7ccefb1d3810c3066b408104c34a28d23
Hill <hillkalola18@gmail.com>
2025-07-10 19:50:30 +0530

    Patterns

---
+++ Patterns/Pattern_16.cpp
@@ -0,0 +1,33 @@
+// pattern 16
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a num : ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << j;
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
